649d14e9aea7e18f237399e2ba17aaaa51e12f50
NicoC54 <nicolas54.consalvi@gmail.com>
2026-09-24 10:28:42 +0200

    Add RGB to HSV conversion and binary segmentation mask code

---
+++ vision_localization/src/Color_spaces_segmentation.cpp
@@ -0,0 +1,54 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(){
+
+    cv::Mat image_bgr = cv::imread("/home/nico/vision_ws/src/ros2_vision_stack/vision_localization/balle_rouge.jpg");
+
+    if (image_bgr.empty()){
+        std::cerr << "Erreur:Image introuvable !" << std::endl;
+        return -1;
+    }
+    else{
+        std::cout << "Image trouvée !" << std::endl;
+        //cv::imshow("Test_balle",image_bgr);
+    }
+
+    cv::Mat image_hsv;
+
+    cv::cvtColor(image_bgr, image_hsv, cv::COLOR_BGR2HSV);
+    cv::imshow("Test_balle",image_hsv);
+
+    cv::Scalar limite_basse1(0,120,70);
+    cv::Scalar limite_haute1(10,255,255);
+
+    cv::Scalar limite_basse2(160,120,70);
+    cv::Scalar limite_haute2(179,250,255);
+
+    cv::Mat masque1, masque2, masque_final;
+
+    cv::inRange(image_hsv, limite_basse1, limite_haute1, masque1);
+    cv::inRange(image_hsv, limite_basse2, limite_haute2, masque2);
+
+    cv::bitwise_or(masque1,masque2,masque_final);
+
+    cv::imshow("image originale bgr", image_bgr);
+    cv::imshow("image hsv", image_hsv);
+    cv::imshow("masque rouge final", masque_final);
+
+    cv::waitKey(0);
+
+
+
+
+
+
+
+
+
+    cv::waitKey(0);
+
+    return 0;
+
+
+}
